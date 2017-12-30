f0fffa65ebece0e0d1d78e959274ee874804b289
LizbethFP <lizbeth.fp@gmail.com>
2017-12-29 22:59:15 -0500

    culminando vista splash

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function begin() {
 function splashScreen() {
   // Crear variable para la sección de carga
   var title = $('.main-title');
-  title.show();
+  $(title).slideDown(3000);
   // Crear parámetro en el que se quita la característica de oculto a la sección de carga
   setTimeout(function() {
     window.location.href = ('views/main-view.html');
