dcbb8fb760f2f3645c6743daeeff51fa98a62e27
LizbethFP <lizbeth.fp@gmail.com>
2017-12-30 14:14:21 -0500

    subiendo modales para restaurantes vegetarianos

---
+++ js/prueba.js
@@ -0,0 +1,58 @@
+
+/* función de prueba */
+// $('.veggie-rest1').on('click', function() {
+//   $('#MODAL-restaurant').find('#slogan').append(getVeggieRestaurantSlogan()[0]);
+//   $('#MODAL-restaurant').find('#address').append(getVeggieRestaurantAddress()[0]);
+//   $('#MODAL-restaurant').find('#services').append(getVeggieRestaurantServices()[0]);
+// });
+
+/*
+// Función que muestra cada información al dar un click en imagen (DE PRUEBA)
+function showInfoByClicking() {
+// Crear variable que obtiene las imágenes
+  var veggieRestPic1 = $('.veggie-rest1');
+  var veggieRestPic2 = $('.veggie-rest2');
+  var veggieRestPic3 = $('.veggie-rest3');
+  var veggieRestPic4 = $('.veggie-rest4');
+  var veggieRestPic5 = $('.veggie-rest5');
+  var veggieRestPic6 = $('.veggie-rest6');
+
+  // Crear función de click para mostrar eslogan para cada restaurante
+  $(veggieRestPic1).on('click', function() {
+    // Crear un párrafo que muestre el slogan del restaurante
+    var restaurantSlogan = $('<span/>');
+    restaurantSlogan.text(getVeggieRestaurantSlogan()[0]);
+    $(this).append(restaurantSlogan);
+  });
+  $(veggieRestPic2).on('click', function() {
+    // Crear un párrafo que muestre el slogan del restaurante
+    var restaurantSlogan = $('<span/>');
+    restaurantSlogan.text(getVeggieRestaurantSlogan()[1]);
+    $(this).append(restaurantSlogan);
+  });
+  $(veggieRestPic3).on('click', function() {
+    // Crear un párrafo que muestre el slogan del restaurante
+    var restaurantSlogan = $('<span/>');
+    restaurantSlogan.text(getVeggieRestaurantSlogan()[2]);
+    $(this).append(restaurantSlogan);
+  });
+  $(veggieRestPic4).on('click', function() {
+    // Crear un párrafo que muestre el slogan del restaurante
+    var restaurantSlogan = $('<span/>');
+    restaurantSlogan.text(getVeggieRestaurantSlogan()[3]);
+    $(this).append(restaurantSlogan);
+  });
+  $(veggieRestPic5).on('click', function() {
+    // Crear un párrafo que muestre el slogan del restaurante
+    var restaurantSlogan = $('<span/>');
+    restaurantSlogan.text(getVeggieRestaurantSlogan()[4]);
+    $(this).append(restaurantSlogan);
+  });
+  $(veggieRestPic6).on('click', function() {
+    // Crear un párrafo que muestre el slogan del restaurante
+    var restaurantSlogan = $('<span/>');
+    restaurantSlogan.text(getVeggieRestaurantSlogan()[5]);
+    $(this).append(restaurantSlogan);
+  });
+}
+*/
--- js/restaurant-info.js
+++ js/restaurant-info.js
@@ -1,68 +1,108 @@
 // Función general que contiene cada función
 function begin() {
-  gettingInfo();
-  showInfoByClicking();
+  gettingVeggieInfo();
+  showVeggieRestInfoByClickingOnModal();
+  // showInfoByClicking();
 }
 
-// Función que muestra cada información al dar un click en imagen (DE PRUEBA)
-function showInfoByClicking() {
-// Crear variable que obtiene las imágenes
-  var veggieRestPic1 = $('.veggie-rest1');
-  var veggieRestPic2 = $('.veggie-rest2');
-  var veggieRestPic3 = $('.veggie-rest3');
-  var veggieRestPic4 = $('.veggie-rest4');
-  var veggieRestPic5 = $('.veggie-rest5');
-  var veggieRestPic6 = $('.veggie-rest6');
-
-  // Crear función de click para mostrar eslogan para cada restaurante
-  $(veggieRestPic1).on('click', function() {
-    // Crear un párrafo que muestre el slogan del restaurante
-    var restaurantSlogan = $('<span/>');
-    restaurantSlogan.text(getRestaurantSlogan()[0]);
-    $(this).append(restaurantSlogan);
-  });
-  $(veggieRestPic2).on('click', function() {
-    // Crear un párrafo que muestre el slogan del restaurante
-    var restaurantSlogan = $('<span/>');
-    restaurantSlogan.text(getRestaurantSlogan()[1]);
-    $(this).append(restaurantSlogan);
-  });
-  $(veggieRestPic3).on('click', function() {
-    // Crear un párrafo que muestre el slogan del restaurante
-    var restaurantSlogan = $('<span/>');
-    restaurantSlogan.text(getRestaurantSlogan()[2]);
-    $(this).append(restaurantSlogan);
-  });
-  $(veggieRestPic4).on('click', function() {
-    // Crear un párrafo que muestre el slogan del restaurante
-    var restaurantSlogan = $('<span/>');
-    restaurantSlogan.text(getRestaurantSlogan()[3]);
-    $(this).append(restaurantSlogan);
-  });
-  $(veggieRestPic5).on('click', function() {
-    // Crear un párrafo que muestre el slogan del restaurante
-    var restaurantSlogan = $('<span/>');
-    restaurantSlogan.text(getRestaurantSlogan()[4]);
-    $(this).append(restaurantSlogan);
-  });
-  $(veggieRestPic6).on('click', function() {
-    // Crear un párrafo que muestre el slogan del restaurante
-    var restaurantSlogan = $('<span/>');
-    restaurantSlogan.text(getRestaurantSlogan()[5]);
-    $(this).append(restaurantSlogan);
+/* FUNCIÓN QUE MUESTRA INFORMACIÓN AL APARECER MODAL PARA RESTAURANTES VEGETARIANOS */
+function showVeggieRestInfoByClickingOnModal() {
+  $('figure').on('click', function() {
+    if ($(this).is('.veggie-rest1')) {
+      // Eliminar el contenido textual de cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').text('');
+      $('#MODAL-restaurant').find('#slogan').text('');
+      $('#MODAL-restaurant').find('#address').text('');
+      $('#MODAL-restaurant').find('#services').text('');
+      // Agregar el contenido textual en cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').append(getVeggieRestaurantName()[0]);
+      $('#MODAL-restaurant').find('#slogan').append(getVeggieRestaurantSlogan()[0]);
+      $('#MODAL-restaurant').find('#address').append(getVeggieRestaurantAddress()[0]);
+      $('#MODAL-restaurant').find('#services').append(getVeggieRestaurantServices()[0]);
+    } else if ($(this).is('.veggie-rest2')) {
+      // Eliminar el contenido textual de cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').text('');
+      $('#MODAL-restaurant').find('#slogan').text('');
+      $('#MODAL-restaurant').find('#address').text('');
+      $('#MODAL-restaurant').find('#services').text('');
+      // Agregar el contenido textual en cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').append(getVeggieRestaurantName()[1]);
+      $('#MODAL-restaurant').find('#slogan').append(getVeggieRestaurantSlogan()[1]);
+      $('#MODAL-restaurant').find('#address').append(getVeggieRestaurantAddress()[1]);
+      $('#MODAL-restaurant').find('#services').append(getVeggieRestaurantServices()[1]);
+    } else if ($(this).is('.veggie-rest3')) {
+      // Eliminar el contenido textual de cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').text('');
+      $('#MODAL-restaurant').find('#slogan').text('');
+      $('#MODAL-restaurant').find('#address').text('');
+      $('#MODAL-restaurant').find('#services').text('');
+      // Agregar el contenido textual en cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').append(getVeggieRestaurantName()[2]);
+      $('#MODAL-restaurant').find('#slogan').append(getVeggieRestaurantSlogan()[2]);
+      $('#MODAL-restaurant').find('#address').append(getVeggieRestaurantAddress()[2]);
+      $('#MODAL-restaurant').find('#services').append(getVeggieRestaurantServices()[2]);
+    } else if ($(this).is('.veggie-rest4')) {
+      // Eliminar el contenido textual de cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').text('');
+      $('#MODAL-restaurant').find('#slogan').text('');
+      $('#MODAL-restaurant').find('#address').text('');
+      $('#MODAL-restaurant').find('#services').text('');
+      // Agregar el contenido textual en cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').append(getVeggieRestaurantName()[3]);
+      $('#MODAL-restaurant').find('#slogan').append(getVeggieRestaurantSlogan()[3]);
+      $('#MODAL-restaurant').find('#address').append(getVeggieRestaurantAddress()[3]);
+      $('#MODAL-restaurant').find('#services').append(getVeggieRestaurantServices()[3]);
+    } else if ($(this).is('.veggie-rest5')) {
+      // Eliminar el contenido textual de cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').text('');
+      $('#MODAL-restaurant').find('#slogan').text('');
+      $('#MODAL-restaurant').find('#address').text('');
+      $('#MODAL-restaurant').find('#services').text('');
+      // Agregar el contenido textual en cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').append(getVeggieRestaurantName()[4]);
+      $('#MODAL-restaurant').find('#slogan').append(getVeggieRestaurantSlogan()[4]);
+      $('#MODAL-restaurant').find('#address').append(getVeggieRestaurantAddress()[4]);
+      $('#MODAL-restaurant').find('#services').append(getVeggieRestaurantServices()[4]);
+    } else if ($(this).is('.veggie-rest6')) {
+      // Eliminar el contenido textual de cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').text('');
+      $('#MODAL-restaurant').find('#slogan').text('');
+      $('#MODAL-restaurant').find('#address').text('');
+      $('#MODAL-restaurant').find('#services').text('');
+      // Agregar el contenido textual en cada elemento
+      $('#MODAL-restaurant').find('#restaurant-name').append(getVeggieRestaurantName()[5]);
+      $('#MODAL-restaurant').find('#slogan').append(getVeggieRestaurantSlogan()[5]);
+      $('#MODAL-restaurant').find('#address').append(getVeggieRestaurantAddress()[5]);
+      $('#MODAL-restaurant').find('#services').append(getVeggieRestaurantServices()[5]);
+    }
   });
 }
 
-// Función para obtener cada información: eslogan, dirección y servicios
-function gettingInfo() {
-  getRestaurantSlogan(); // devuelve array   getRestaurantSlogan()[0]
-  getRestaurantAddress();
-  getRestaurantServices();
+/* FUNCIÓN PARA OBTENER CADA INFORMACIÓN DE RESTAURANTE VEGETARIANO: ESLOGAN, DIRECCIÓN Y SERVICIOS */
+function gettingVeggieInfo() {
+  getVeggieRestaurantName();
+  getVeggieRestaurantSlogan(); // devuelve array   getVeggieRestaurantSlogan()[0]
+  getVeggieRestaurantAddress();
+  getVeggieRestaurantServices();
 }
 
+/* Función para obtener nombre del restaurante vegetariano (devuelve array) */
+function getVeggieRestaurantName(data) {
+  // alert('Esta función debe conectarse con el mouseover');
+  //  Crear variable para tipo de restaurante (vegetariano)
+  var typeofrestaurant = dataRestaurants['vegetarian-restaurants'];
+  // Crear variable para capturar la lista de restaurantes tomándolo de la data de restaurates
+  var veggieRestaurantNames = [];
+  // Iterar por cada uno de los restaurantes de comida vegetariana para conseguir el nombre de cada uno
+  for (var i = 0; i < typeofrestaurant.length; i++) {
+    name = typeofrestaurant[i]['name'];
+    veggieRestaurantNames.push(name);
+  }
+  return veggieRestaurantNames;
+}
 
-/* FUNCIÓN QUE OBTIENE UN ARRAY CON LOS ESLÓGANES */
-function getRestaurantSlogan(data) {
+/* FUNCIÓN QUE OBTIENE UN ARRAY CON LOS ESLÓGANES DE RESTAURANTES VEGETARIANOS */
+function getVeggieRestaurantSlogan(data) {
   //  Crear variable para tipo de restaurante (vegetariano)
   var typeofrestaurant = dataRestaurants['vegetarian-restaurants'];
   // Crear variable para capturar la lista de restaurantes tomándolo de la data de restaurates
@@ -75,8 +115,8 @@ function getRestaurantSlogan(data) {
   return restaurantSlogans;
 }
 
-/* FUNCIÓN QUE OBTIENE UN ARRAY CON LAS DIRECCIONES */
-function getRestaurantAddress(data) {
+/* FUNCIÓN QUE OBTIENE UN ARRAY CON LAS DIRECCIONES DE RESTAURANTES VEGETARIANOS */
+function getVeggieRestaurantAddress(data) {
   //  Crear variable para tipo de restaurante (vegetariano)
   var typeofrestaurant = dataRestaurants['vegetarian-restaurants'];
   // Crear variable para capturar la lista de restaurantes tomándolo de la data de restaurates
@@ -89,8 +129,8 @@ function getRestaurantAddress(data) {
   return restaurantAddress;
 }
 
-/* FUNCIÓN QUE OBTIENE UN ARRAY CON LOS SERVICIOS OFRECIDOS POR RESTAURANTES */
-function getRestaurantServices(data) {
+/* FUNCIÓN QUE OBTIENE UN ARRAY CON LOS SERVICIOS OFRECIDOS POR RESTAURANTES VEGETARIANOS */
+function getVeggieRestaurantServices(data) {
   //  Crear variable para tipo de restaurante (vegetariano)
   var typeofrestaurant = dataRestaurants['vegetarian-restaurants'];
   // Crear variable para capturar la lista de restaurantes tomándolo de la data de restaurates
