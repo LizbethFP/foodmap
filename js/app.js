bdb4cab49c0c3d79fc3326c262c33ba0a034b4d0
LizbethFP <lizbeth.fp@gmail.com>
2017-12-29 23:22:32 -0500

    probando toggle en vista splash

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function begin() {
 function splashScreen() {
   // Crear variable para la sección de carga
   var title = $('.main-title');
-  $(title).slideDown(3000);
+  $(title).slideToggle(3000);
   // Crear parámetro en el que se quita la característica de oculto a la sección de carga
   setTimeout(function() {
     window.location.href = ('views/main-view.html');
